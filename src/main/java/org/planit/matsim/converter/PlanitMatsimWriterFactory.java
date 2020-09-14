52ceb73a1093f30e14aa98d131b22931b81144bc
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2020-09-14 16:43:04 +1000

    first checkin, empty skeleton classes for MATSIM writer added

---
+++ src/main/java/org/planit/matsim/converter/PlanitMatsimWriterFactory.java
@@ -0,0 +1,19 @@
+package org.planit.matsim.converter;
+
+/**
+ * Factory for creating PLANitMatsimWriters
+ * @author markr
+ *
+ */
+public class PlanitMatsimWriterFactory {
+  
+  /** Create a PLANitMatsimWriter which persists PLANit networks in MATSIM network format
+   * 
+   * @param outputDirectory to use
+   * @return create matsim writer
+   */
+  public static PlanitMatsimWriter createWriter(String outputDirectory) {
+    return new PlanitMatsimWriter();    
+  }
+    
+}
