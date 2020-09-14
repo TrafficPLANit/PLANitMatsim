dfd0c330271dcf04d7528fecdfa82b1ea4ac93bd
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2020-09-14 16:43:04 +1000

    first checkin, empty skeleton classes for MATSIM writer added

---
+++ Release_Log.md
@@ -0,0 +1,6 @@
+# Release Log
+
+PlanIt Releases
+
+This project contains core code to write a MATSIM network to disk based on PLANit memory model
+
---
+++ src/main/java/org/planit/matsim/converter/PlanitMatsimWriter.java
@@ -0,0 +1,22 @@
+package org.planit.matsim.converter;
+
+import org.planit.network.converter.NetworkWriter;
+import org.planit.network.physical.macroscopic.MacroscopicNetwork;
+
+/**
+ * A class that takes a PLANit network and writes it as a MATSIM network
+ * 
+ * @author markr
+  */
+public class PlanitMatsimWriter implements NetworkWriter {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void write(MacroscopicNetwork network) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
