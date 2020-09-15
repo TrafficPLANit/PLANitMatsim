bc4ea034327ecd0eb935c0ae3c0541be5339ce36
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2020-09-15 20:07:47 +1000

    - working on MATSIM network writer. NOT DONE

---
+++ src/main/java/org/planit/matsim/xml/MatsimNetworkXmlAttributes.java
@@ -0,0 +1,45 @@
+package org.planit.matsim.xml;
+
+/**
+ * MATSIM network XML attributes used
+ * 
+ * @author markr
+ *
+ */
+public class MatsimNetworkXmlAttributes {
+  
+  public static final String ID = "id";
+  
+  public static final String TYPE = "type";
+  
+  public static final String ORIGID = "origid";  
+  
+  /* node specific */
+  
+  public static final String X = "x";  
+
+  public static final String Y = "y";
+  
+  public static final String Z = "z";
+    
+  /* link specific */
+
+  public static final String FROM = "from";
+  
+  public static final String TO = "to";
+
+  public static final String LENGTH = "length";
+  
+  public static final String NT_CATEGORY = "nt_category";
+  
+  public static final String NT_TYPE = "nt_type";
+  
+  public static final String FREESPEED_METER_SECOND = "freespeed";
+
+  public static final String CAPACITY_HOUR = "capacity";
+
+  public static final String PERMLANES = "permlanes";
+
+  public static final String MODES = "modes";
+  
+}
