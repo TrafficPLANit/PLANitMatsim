26ed7a9fb3b3f46a77e21ccb69c368a7c129679c
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2020-11-24 19:27:01 +1100

    - support updates to country names in reader/writer

--- src/main/java/org/planit/matsim/converter/PlanitMatsimWriterFactory.java
+++ src/main/java/org/planit/matsim/converter/PlanitMatsimWriterFactory.java
@@ -15,5 +15,16 @@ public class PlanitMatsimWriterFactory {
   public static PlanitMatsimWriter createWriter(String outputDirectory) {
     return new PlanitMatsimWriter(outputDirectory);    
   }
+  
+  /** Create a PLANitMatsimWriter which persists PLANit networks in MATSIM network format
+   * 
+   * @param outputDirectory to use
+   * @param countryName country which the input file represents, used to determine defaults in case not specifically specified in OSM data, when left blank global defaults will be used
+   * based on a right hand driving approach
+   * @return create matsim writer
+   */
+  public static PlanitMatsimWriter createWriter(String outputDirectory, String countryName) {
+    return new PlanitMatsimWriter(outputDirectory, countryName);    
+  }  
     
 }
